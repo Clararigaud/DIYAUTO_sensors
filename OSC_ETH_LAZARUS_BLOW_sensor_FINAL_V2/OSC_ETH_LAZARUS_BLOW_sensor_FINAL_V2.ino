68c92993f9bcf2fa54a00895fc99ce4667e008c8
Julien RAT <julienrat@riseup.net>
2024-09-20 09:41:32 +0200

    modification de Piezo, Blow et config windy

--- OSC_ETH_LAZARUS_BLOW_sensor_FINAL_V2/OSC_ETH_LAZARUS_BLOW_sensor_FINAL_V2.ino
+++ OSC_ETH_LAZARUS_BLOW_sensor_FINAL_V2/OSC_ETH_LAZARUS_BLOW_sensor_FINAL_V2.ino
@@ -2,14 +2,20 @@
 #include <ArduinoOSCETH.h>
 #include <ADS1115_WE.h>
 #include <Wire.h>
+#include <DataTome.h>
 #include <Ticker.h>
 
+// Average Lib
+DataTomeMvAvg<unsigned, unsigned long> average_1(25);
+
+//ADC converter I2C
 #define I2C_ADDRESS_1 0x48
 #define I2C_ADDRESS_2 0x49
 
 ADS1115_WE adc_1 = ADS1115_WE(I2C_ADDRESS_1);
 ADS1115_WE adc_2 = ADS1115_WE(I2C_ADDRESS_2);
 
+// Average Lib
 Ticker Wake_up;
 // SD MMC lib
 #include "FS.h"
@@ -44,12 +50,6 @@ int publish_port = 7001;
 // send / receive varibales
 int i;
 
-//piezo variables
-
-int trigger = 400;
-int piezo_value = 0;
-float a0, a1, a2, a3, a4;
-
 //Config file
 String config_array[100];
 String path;
@@ -58,12 +58,33 @@ String path2;
 String path3;
 String path4;
 String path5;
-int gain1 = 0;
-int gain2 = 0;
-int gain3 = 0;
-int gain4 = 0;
-int gain5 = 0;
+String path6;
+String path7;
+String path8;
+int gain8 = 0;
+float msg = 0.0;
+float msg1 = 0.0;
+float msg2 = 0.0;
+float msg3 = 0.0;
+float msg4 = 0.0;
+float msg5 = 0.0;
+float msg6 = 0.0;
+float msg7 = 0.0;
+float msg8 = 0.0;
 int refresh = 500;
+int nb_ech = 50;
+int threshold = 200;
+float etal;
+float humidity;
+long value_1;
+boolean first_etal = true;
+
+float msg1A = 0.0;
+float msg2A = 0.0;
+float msg3A = 0.0;
+float msg4A = 0.0;
+float msg5A = 0.0;
+float msg6A = 0.0;
 
 IPAddress stringToIPAddress(String ipAddressString) {
   // Split the string into four parts using '.' as delimiter
@@ -114,15 +135,21 @@ void readConfigFile(fs::FS& fs, const char* path) {
   path3 = config_array[8];
   path4 = config_array[9];
   path5 = config_array[10];
-  gain1 = config_array[11].toInt();
-  gain2 = config_array[12].toInt();
-  gain3 = config_array[13].toInt();
-  gain4 = config_array[14].toInt();
-  gain5 = config_array[15].toInt();
-  reset_interval = config_array[16].toInt();
+  path6 = config_array[11];
+  path7 = config_array[12];
+  gain8 = config_array[13].toInt();
+  msg1 = config_array[14].toFloat();
+  msg2 = config_array[15].toFloat();
+  msg3 = config_array[16].toFloat();
+  msg4 = config_array[17].toFloat();
+  msg5 = config_array[18].toFloat();
+  msg6 = config_array[19].toFloat();
+  msg7 = config_array[20].toFloat();
+  msg8 = config_array[21].toFloat();
+  nb_ech = config_array[23].toInt();
+  threshold = config_array[22].toInt();
+  reset_interval = config_array[24].toInt();
   interval = reset_interval * 60 * 1000;
-
-
 
   Serial.print("Ip : ");
   Serial.println(ip);
@@ -141,19 +168,78 @@ void readConfigFile(fs::FS& fs, const char* path) {
   Serial.print("path 3 : ");
   Serial.println(path3);
   Serial.print("path 4 : ");
-  Serial.println(path5);
+  Serial.println(path4);
   Serial.print("path 5 : ");
-  Serial.println(path1);
+  Serial.println(path5);
+  Serial.print("path 6 : ");
+  Serial.println(path6);
+  Serial.print("path 7 : ");
+  Serial.println(path7);
+  Serial.print("gain 8 : ");
+  Serial.println(gain8);
+  Serial.print("msg 1 : ");
+  Serial.println(msg1);
+  Serial.print("msg 2 : ");
+  Serial.println(msg2);
+  Serial.print("msg 3 : ");
+  Serial.println(msg3);
+  Serial.print("msg 4 : ");
+  Serial.println(msg4);
+  Serial.print("msg 5 : ");
+  Serial.println(msg5);
+  Serial.print("msg 6 : ");
+  Serial.println(msg6);
+  Serial.print("msg 7 : ");
+  Serial.println(msg7);
+  Serial.print("msg 8 : ");
+  Serial.println(msg8);
   Serial.print("refresh frequency in millis : ");
   Serial.println(refresh);
-  Serial.print("Interval reset in millis: ");
-  Serial.println(interval);
+  Serial.print("nombre echantillons : ");
+  Serial.println(nb_ech);
+  Serial.print("threshold : ");
+  Serial.println(threshold);
 }
-
+/*
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}*/
 void Wake() {
   OscEther.update();
 }
-
 void setup() {
 
   Wire.begin();
@@ -162,14 +248,16 @@ void setup() {
   delay(2000);
   //Read config.ini file in SD MMC card
 
+
+
   if (!SD_MMC.begin()) {
     Serial.println("Card Mount Failed");
     return;
   }
 
-  readConfigFile(SD_MMC, "/config_piezo.ini");
+  readConfigFile(SD_MMC, "/config_windy.ini");
   if (debug_juju) {
-    host = "192.168.0.52";
+    host = "192.168.0.53";
     path1 = "/test";
   }
 
@@ -190,7 +278,7 @@ void setup() {
   adc_2.setVoltageRange_mV(ADS1115_RANGE_6144);
 
   // Ethernet stuff
-
+  // WiFi.onEvent(WiFiEvent);
   ETH.begin();
   ETH.begin(ip, gateway, subnet);
   Serial.print("Host Target Ip : ");
@@ -200,73 +288,124 @@ void setup() {
   Serial.print("path 1 : ");
   Serial.println(path1);
 
-  OscEther.publish(host, publish_port, path1, a0)
+  OscEther.publish(host, publish_port, path1, msg)
     ->setIntervalMsec(refresh);
-  OscEther.publish(host, publish_port, path2, a1)
+  OscEther.publish(host, publish_port, path2, msg1A)
     ->setIntervalMsec(refresh);
-  OscEther.publish(host, publish_port, path3, a2)
+  OscEther.publish(host, publish_port, path3, msg2A)
     ->setIntervalMsec(refresh);
-  OscEther.publish(host, publish_port, path4, a3)
+  OscEther.publish(host, publish_port, path4, msg3A)
     ->setIntervalMsec(refresh);
-  OscEther.publish(host, publish_port, path5, a4)
+  OscEther.publish(host, publish_port, path5, msg4A)
     ->setIntervalMsec(refresh);
-  // OscEther.publish(host, publish_port, path, piezo_value)
-  // ->setIntervalMsec(10);
+  OscEther.publish(host, publish_port, path6, msg5A)
+    ->setIntervalMsec(refresh);
+  OscEther.publish(host, publish_port, path7, msg6A)
+    ->setIntervalMsec(refresh);
 
-  const char* host_juju = "192.168.1.13";
-  OscEther.publish(host_juju, publish_port, "/coucou", a0)
-    ->setIntervalMsec(refresh);
+
+  //Etallonnage des capteurs
 }
 
 void loop() {
-  // mise en route du timer reset
+   // mise en route du timer reset
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     ESP.restart();
   }
+  int thresold = 2000;
   //Lecture des ADC
-  a0 = map(readChannel_1(ADS1115_COMP_0_GND), 0, gain1, 0, 1);
-  a1 = map(readChannel_1(ADS1115_COMP_1_GND), 0, gain2, 0, 1);
-  a2 = map(readChannel_1(ADS1115_COMP_2_GND), 0, gain3, 0, 1);
-  a3 = map(readChannel_1(ADS1115_COMP_3_GND), 0, gain4, 0, 1);
-  a4 = map(readChannel_2(ADS1115_COMP_0_GND), 0, gain5, 0, 1);
-  if (a0 > 1) {
-    a0 = 1;
+  if (readChannel_1(ADS1115_COMP_0_GND) > thresold) {
     previousMillis = currentMillis;
-  }
-  if (a1 > 1) {
-    a1 = 1;
-    previousMillis = currentMillis;
-  }
-  if (a2 > 1) {
-    a2 = 1;
-    previousMillis = currentMillis;
-  }
-  if (a3 > 1) {
-    a3 = 1;
-    previousMillis = currentMillis;
-  }
-  if (a4 > 1) {
-    a4 = 1;
-    previousMillis = currentMillis;
+    msg1A = msg1;
+    path = path2;
+
+    Serial.println("bt 1");
+  } else {
+    msg1A = 0;
   }
 
-  if (a0 < 0) {
-    a0 = 0;
+  if (readChannel_1(ADS1115_COMP_1_GND) > thresold) {
+    previousMillis = currentMillis;
+    //send msg
+    msg2A = msg2;
+    path = path3;
+
+    Serial.println("bt 2");
+  } else {
+    msg2A = 0;
   }
-  if (a1 < 0) {
-    a1 = 0;
+
+  if (readChannel_1(ADS1115_COMP_2_GND) > thresold) {
+    previousMillis = currentMillis;
+    //send msg
+    msg3A = msg3;
+
+    Serial.println("bt 3");
+  } else {
+    msg3A = 0;
   }
-  if (a2 < 0) {
-    a2 = 0;
+
+  if (readChannel_1(ADS1115_COMP_3_GND) > thresold) {
+    previousMillis = currentMillis;
+    //send msg
+    msg4A = msg4;
+
+    Serial.println("bt 4");
+  } else {
+    msg4A = 0;
   }
-  if (a3 < 0) {
-    a3 = 0;
+  if (readChannel_2(ADS1115_COMP_0_GND) > thresold) {
+    previousMillis = currentMillis;
+    //send msg
+    msg5A = msg5;
+
+    Serial.println("bt 5");
+  } else {
+    msg5A = 0;
   }
-  if (a4 < 0) {
-    a4 = 0;
+
+  if (readChannel_2(ADS1115_COMP_1_GND) > thresold) {
+    previousMillis = currentMillis;
+    //send msg
+    msg6A = msg6;
+
+    Serial.println("bt 6");
+  } else {
+    msg6A = 0;
   }
-  OscEther.post();  // to publish osc
+  /*
+  if (aht20.available() == true)
+  {
+    humidity = aht20.getHumidity();
+    averageHum_1.push(humidity);
+    if (first_etal) {
+      etal = averageHum_1.get();
+      first_etal = false;
+    }
+    value_1 = averageHum_1.get() - etal ;
+    // Serial.println(value_1);
+
+  }
+*/
+  value_1 = 0;
+  for (int i = 0; i < nb_ech; i++) {
+    value_1 = value_1 + readChannel_2(ADS1115_COMP_2_GND);
+  }
+  value_1 = value_1 / nb_ech;
+  if (value_1 < threshold) {
+    value_1 = 0;
+  }else{
+    previousMillis = currentMillis;
+  }
+  msg = constrain(map(value_1, threshold, 1000, 0, 100), 0, 100) / 100.0;
+
+  path = path1;
+  Serial.println(msg);
+
+
+
+  OscEther.post();
 }
 
 
